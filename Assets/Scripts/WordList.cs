3cc07caf96134c01672fbf2a85bb88b22a38ec68
maxi-ht <mcacace@untref.edu.ar>
2024-11-23 00:41:57 -0300

    Primer commit

---
+++ Assets/Scripts/WordList.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordList : MonoBehaviour
+{
+    public static readonly List<string> AcceptableWords = new List<string>
+    {
+        "words",
+    };
+}
